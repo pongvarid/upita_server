456ff75776d0ce94e1baa33ac08f5db92dca9c06
pongvarid <pongvarid@gmail.com>
2021-02-06 16:19:06 +0700

    Add rate

--- report/views.py
+++ report/views.py
@@ -1,16 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
 from report.serializers import ReportAllSerializer, ReportDetailSerializer, ReportRawIITSerializer, ReportRawEITSerializer
 from report.models import ReportAll, ReportDetail, ReportRawIIT, ReportRawEIT
-
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ReportAllViewSet(ModelViewSet):
     queryset = ReportAll.objects.order_by('pk')
     serializer_class = ReportAllSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ['agency', 'year','rate']
+    search_fields = ['agency','year','rate']
 
 
 class ReportDetailViewSet(ModelViewSet):
     queryset = ReportDetail.objects.order_by('pk')
     serializer_class = ReportDetailSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ['agency', 'year','name']
+    search_fields = ['agency', 'year', 'name']
 
 
 class ReportRawIITViewSet(ModelViewSet):
