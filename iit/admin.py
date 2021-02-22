91dd5e14ec6cbaf758343c002b487d0679dd343e
pongvarid <pongvarid@gmail.com>
2021-02-23 00:36:20 +0700

    add test api2

--- iit/admin.py
+++ iit/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter
+from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter,RelatedOnlyDropdownFilter
 
 # Register your models here.
 from iit.models import  *
@@ -56,16 +56,19 @@ class AnswerIssueAdmin(admin.ModelAdmin):
     search_fields = ['assessmentIssues__name','agency__name','issue__name','user__email','user__username','user__first_name','user__last_name']
     # autocomplete_fields = ['name',]
     ordering = ('issue__order',)
-    list_display = ( 'agency_name','assessmentIssues_name','issue_name','issueDetail_name','value_by','user_name','user_email')
+    list_display = ( 'agency_name','user_student','assessmentIssues_name','issue_name','issueDetail_name','value_by','user_name','user_email')
     # list_filter = ('agency','assessmentIssues','user','issue','value_by')
     list_filter = (
         ('agency', RelatedDropdownFilter),
+        # 'user_student',
         ('assessmentIssues', RelatedDropdownFilter),
         # ('user', RelatedDropdownFilter),
         ('issue', RelatedDropdownFilter),
         ('value_by', DropdownFilter),
         ('year', DropdownFilter),
     )
+
+
 
 admin.site.register(AnswerIssue,AnswerIssueAdmin)
 
@@ -80,11 +83,33 @@ class AnswerSuggestionAdmin(admin.ModelAdmin):
 admin.site.register(AnswerSuggestion,AnswerSuggestionAdmin)
 # admin.site.register(IssueDetail)
 
+class IsVeryBenevolentFilter(admin.SimpleListFilter):
+    title = 'ใครเป็นนิสิต'
+    parameter_name = 'is_very_benevolent'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Yes', 'Yes'),
+            ('No', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        profile = Profile.objects.filter(agency=41).values_list('user_id', flat=True)
+        if value == 'Yes':
+
+            return queryset.filter(user__in=profile)
+        elif value == 'No':
+            return queryset.exclude(user__in=profile)
+        return queryset
+
 class UserInAnswerAdmin(admin.ModelAdmin):
     search_fields = [ 'agency__name','user__first_name','user__last_name','user__email']
-    list_display = ( 'agency_name' ,'user_name','user_email', 'year','created_at')
-    list_filter = ('agency',)
+    list_display = ( 'agency_name' ,'user_student','user_name','user_email', 'year','created_at')
+    # list_filter = ('agency',)
     list_filter = (
+        # 'user_student',
+        IsVeryBenevolentFilter,
         ('agency', RelatedDropdownFilter),
         ('year',DropdownFilter),)
 admin.site.register(UserInAnswer,UserInAnswerAdmin)
