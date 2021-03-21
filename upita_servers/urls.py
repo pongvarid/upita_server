cf5a9259e22e716f10d915f519c4ad009aee7cc0
pongv <pongvarid@gmail.com>
2021-03-22 03:25:46 +0700

    add web config url

--- upita_servers/urls.py
+++ upita_servers/urls.py
@@ -22,6 +22,7 @@ from django.urls import re_path
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
+    path('setting/',include("web.urls")),
     path('api/ita/v1/',include("ita.api.urls")),  
     path('api/ita/v2/',include("ita.apiv2.urls")),
     path('rest-auth/', include('rest_auth.urls')),
