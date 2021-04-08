969117f7623741ad26f8d1fe8807269aa993e0f5
pongv <pongvarid@gmail.com>
2021-04-08 23:40:39 +0700

    add type

--- oit/views.py
+++ oit/views.py
@@ -8,5 +8,5 @@ class EvaluateOITViewSet(ModelViewSet):
     queryset = EvaluateOIT.objects.order_by('pk')
     serializer_class = EvaluateOITSerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
-    filterset_fields = ['rate', 'rate__year','agency','rate_status']
+    filterset_fields = ['rate','rate__type','rate__type_base', 'rate__year','agency','rate_status']
     search_fields = ['rate__name','agency__name']
